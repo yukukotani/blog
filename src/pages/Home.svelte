5dbf5ec856b9ace85e25ad7d6e94c0af006acc42
Monchi <poyo0315@gmail.com>
2019-06-07 19:24:56 +0900

    Format date

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,5 +1,6 @@
 <script>
   import { Link } from "svelte-routing";
+  import format from "date-fns/format";
   import { posts } from "../stores/post.js"
 
   let items = null;
@@ -7,6 +8,10 @@
   posts.subscribe(value => {
     items = value;
   })
+
+  function formatDate(raw) {
+    return raw
+  }
 </script>
 <style scoped>
 </style>
@@ -15,7 +20,7 @@
     <h1>読み込み中</h1>
   {:else}
     {#each items as post}
-      <Link to="posts/{post.sys.id}">{post.fields.title} - {post.sys.createdAt}</Link>
+      <Link to="posts/{post.sys.id}">{post.fields.title} - {format(post.sys.createdAt, "YYYY/MM/DD")}</Link>
     {/each}
   {/if}
 </div>
